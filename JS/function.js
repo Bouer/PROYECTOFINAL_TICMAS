2037eb08ad0e13ccb1cc37e6b5dc3280b65fbade
MatiasBouer <70662198+Bouer@users.noreply.github.com>
2023-02-22 00:05:03 -0300

    Modificando mobile y tablet, casi listo

--- JS/function.js
+++ JS/function.js
@@ -1,23 +1,13 @@
-// set the target element that will be collapsed or expanded (eg. navbar menu)
-const $targetEl = document.getElementById('targetEl');
-
-// optionally set a trigger element (eg. a button, hamburger icon)
-const $triggerEl = document.getElementById('triggerEl');
-
-// optional options with default values and callback functions
-const options = {
-  onCollapse: () => {
-      console.log('element has been collapsed')
-  },
-  onExpand: () => {
-      console.log('element has been expanded')
-      console.log("HOLA MUNDO")
-  },
-  onToggle: () => {
-      console.log('element has been toggled')
-      console.log("HOLA MUNDO")
-  }
-};
+const menu = document.querySelector(".menu");
+const  openMenuBtn = document.querySelector(".open-menu");
 
 
-console.log("HOLA MUNDO")
+/*console.log("HOLA MUNDO"); //Prueba de que funciona */
+
+
+function toggleMenu (){
+    menu.classList.toggle("menu_opened");
+}
+
+openMenuBtn.addEventListener("click", toggleMenu);
+closeMenuBtn.addEventListener("click", toggleMenu);
